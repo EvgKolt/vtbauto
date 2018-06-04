60a2ec8fc6fcf93afd1a97dfe3c6c25b60ce8ca3
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-06-04 10:10:42 +0300

    change class propertypath on constants(place constants here)

---
+++ src/main/java/ru/open/entities/Constants.java
@@ -0,0 +1,6 @@
+package ru.open.entities;
+
+//todo change private on public and delete getter and classname on constans
+public final class Constants {
+    public static final String PROPERTY_PATH = "src/main/resources/properties.properties";
+}
