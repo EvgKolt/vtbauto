ab9a7383dba54630d48f24a97fd2239d634c3392
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-06-07 17:58:26 +0300

    123

--- src/main/java/ru/open/parsersandhelpers/TextGenerator.java
+++ src/main/java/ru/open/parsersandhelpers/TextGenerator.java
@@ -1,13 +1,14 @@
 package ru.open.parsersandhelpers;
 
 
-import java.io.IOException;
+import java.util.UUID;
 
 public final class TextGenerator {
 
-
-    public static void main(String[] args) throws IOException {
-        LogParser logParser = new LogParser();
-        System.out.println(logParser.getLastSmsCode());
+    public String generatePassword() {
+        int subIndex = 20;
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "").substring(subIndex);
+        return "!Qw" + uuid;
     }
+
 }
