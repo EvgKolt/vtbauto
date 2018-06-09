9caec9581a1a0b13f62b58abc15323d26a5b8cee
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-06-09 10:16:37 +0300

    123

--- src/test/java/ru/open/runners/BusinessPortalTest.java
+++ src/test/java/ru/open/runners/BusinessPortalTest.java
@@ -11,7 +11,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -34,8 +33,6 @@ public class BusinessPortalTest {
     @BeforeClass
     static public void setupTimeout() throws MalformedURLException {
 
-        Dimension d = new Dimension(1382, 744);
-        //Resize the current window to the given dimension
         /////////////////////////////////for remote runs////////////////////////////////
         Configuration.remote = "http://10.52.185.105:4419/wd/hub";
         Configuration.browser = "chrome";
@@ -44,14 +41,11 @@ public class BusinessPortalTest {
         capabilities.setCapability(ACCEPT_SSL_CERTS, true);
         WebDriver wd = new RemoteWebDriver(new URL("http://10.52.185.105:4419/wd/hub"), capabilities);
         setWebDriver(wd);
-        wd.manage().window().setSize(d);
 
-        /*
         ///////////////////////////////////for local runs////////////////////////////
-        Configuration.timeout = 10000;
-        //Webdriver configuration - optional
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver.exe");
-        Configuration.browser = "chrome";*/
+        //Configuration.timeout = 10000;
+        //System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver.exe");
+        //Configuration.browser = "chrome";
 
     }
 }
