4e990c126bcae67a374a1f3ba5748e5d69458376
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-06-13 09:51:47 +0300

    refresh BPT

--- src/test/java/ru/open/runners/BusinessPortalTest.java
+++ src/test/java/ru/open/runners/BusinessPortalTest.java
@@ -1,19 +1,10 @@
 package ru.open.runners;
 
-import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
-import static org.openqa.selenium.remote.CapabilityType.ACCEPT_SSL_CERTS;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import com.codeborne.selenide.Configuration;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * plugin – reporting
@@ -26,26 +17,26 @@ import org.openqa.selenium.remote.RemoteWebDriver;
         plugin = {"html:target/cucumber-report/businessportaltest", "json:target/cucumber.json"},
         features = "src/test/java/ru/open/features",
         glue = "ru/open/steps",
-        tags = "@businessportaltest"
+        tags = "@last"
 )
 
 public class BusinessPortalTest {
     @BeforeClass
-    static public void setupTimeout() throws MalformedURLException {
+    static public void setupTimeout() {
 
         /////////////////////////////////for remote runs////////////////////////////////
-        Configuration.remote = "http://10.52.185.105:4419/wd/hub";
-        Configuration.browser = "chrome";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setCapability(ACCEPT_SSL_CERTS, true);
-        WebDriver wd = new RemoteWebDriver(new URL("http://10.52.185.105:4419/wd/hub"), capabilities);
-        setWebDriver(wd);
+        //        Configuration.remote = "http://10.52.185.105:4419/wd/hub";
+        //        Configuration.browser = "chrome";
+        //        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //        capabilities.setBrowserName("chrome");
+        //        capabilities.setCapability(ACCEPT_SSL_CERTS, true);
+        //        WebDriver wd = new RemoteWebDriver(new URL("http://10.52.185.105:4419/wd/hub"), capabilities);
+        //        setWebDriver(wd);
 
         ///////////////////////////////////for local runs////////////////////////////
-        //Configuration.timeout = 10000;
-        //System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver.exe");
-        //Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver.exe");
+        Configuration.browser = "chrome";
 
     }
 }
