f43719ab17dddab7873af5e9084f6a6cc4e23d48
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-07-02 14:41:30 +0300

    change property path

--- src/main/java/ru/open/Constants.java
+++ src/main/java/ru/open/Constants.java
@@ -2,8 +2,8 @@ package ru.open;
 
 
 public final class Constants {
-    public static final String PROPERTY_PATH = "src/main/resources/properties.properties";
-
+    public static final String PROPERTY_PATH =
+            "C:\\Jenkins513\\workspace\\MSB-Portal\\CucumberTests\\properties.properties";
     private Constants() {
     }
 }
