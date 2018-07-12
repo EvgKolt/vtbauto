bf870672397c92820aedcde033b03bf61557e07f
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-07-12 16:15:33 +0300

    updateProperties

--- src/main/java/ru/open/pageobjects/shugar/ShugarActionPage.java
+++ src/main/java/ru/open/pageobjects/shugar/ShugarActionPage.java
@@ -16,5 +16,25 @@ public class ShugarActionPage extends AbstractPage {
 
     @FindBy(id = "search_form_submit")
     private SelenideElement search;
+
+    @FindBy(id = "tab_link_MoneyAccount|advanced_search")
+    private SelenideElement searchAdvanced;
+
+    @FindBy(xpath = "//*[contains(text(), '\n"
+            + "В работе ООС (РСЦ)')]")
+    private SelenideElement inWork;
+
+    @FindBy(xpath = "//a[@title='Изменить']")
+    private SelenideElement changeStatus;
+
+    @FindBy(xpath = "//select[@id='status']")
+    private SelenideElement statusMenu;
+
+    @FindBy(xpath = "//select/option[@value='waiting_info']")
+    private SelenideElement statusMenuOption1;
+
+    @FindBy(xpath = "//form[@id='set_status_frm']/input[contains(@class, 'button')]")
+    private SelenideElement saveStatus;
+
 }
 
