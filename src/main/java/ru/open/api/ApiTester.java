2f6c4cc5efd0841e291fc02e5d76f445379285b6
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-07-16 09:31:20 +0300

    update variable URI

--- src/main/java/ru/open/api/ApiTester.java
+++ src/main/java/ru/open/api/ApiTester.java
@@ -5,11 +5,11 @@ import static com.jayway.restassured.RestAssured.given;
 public class ApiTester {
     //howToGetSSO->Select * from tb_organization where inn = '*********';
 
-    public void checkStatusCodeGET(String URI) {
+    public void checkStatusCodeGET(String uri) {
         //header helps to ignore authorisation
         given().
                 header("Authorization", "Bearer dev_1.0_sso_____00a596c0-30ea-484c-8901-cefe36c31799").
-                when().get(URI).then().statusCode(200);
+                when().get(uri).then().statusCode(200);
     }
 
     public void checkResponseBody() {
