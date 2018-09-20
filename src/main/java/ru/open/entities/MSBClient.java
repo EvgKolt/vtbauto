465da20128f7bfd2a7a84343d7a777ccd6fede51
koltunovskiy_ea <evgeniy.koltunovskiy@open.ru>
2018-09-20 15:09:51 +0300

    update

--- src/main/java/ru/open/entities/MSBClient.java
+++ src/main/java/ru/open/entities/MSBClient.java
@@ -2,7 +2,7 @@ package ru.open.entities;
 
 
 public final class MSBClient {
-    public static final String ORGANIZATION_ID = "'8c162fa0-4b58-4e0a-8590-20cd9b3ef1cd'";
+    public static final String ORGANIZATION_ID = "8c162fa0-4b58-4e0a-8590-20cd9b3ef1cd";
     public static final int CONTACT_ID = 4798;
 
     private MSBClient() {
