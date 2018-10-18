3ad7d45fb79ba4f2825bbb553d5e4291ba018bce
koltunovskiy_ea <dandelionR14>
2018-10-18 18:01:38 +0300

    update

--- src/main/java/ru/open/entities/MSBClient.java
+++ src/main/java/ru/open/entities/MSBClient.java
@@ -2,7 +2,8 @@ package ru.open.entities;
 
 
 public final class MSBClient {
-    public static final String ORGANIZATION_ID = "b840b28b-b60a-4393-bfe6-e8ee3b7c3647";
+    public static final String ORGANIZATION_ID = "d218ed7b-4f04-429b-b053-773edbcb7645";
+    public static final String ORGANIZATION_ID_CARD = "9244ef6e-f487-426e-ae36-33861a8e1e24";
     //for card replenishment
     public static final String ACC_LAST_NUMBER = "*0031";
     public static final int CONTACT_ID = 4824;
